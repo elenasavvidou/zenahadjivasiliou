c18601fd89668912e2604b6f1836d57ecfd9462e
elenasavvidou <savvidou.eleni@gmail.com>
2019-01-14 10:48:18 +0100

    added htmls and fixed css /public

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + './public'));
+app.use(express.static(path.join(__dirname, '/public')));Fig6-1.jpg
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, './index.html'));
